734d1341e6d2c83c225f94cd8b2702ec2df3dec9
Julien 🖖 <127381239+newmanjulien@users.noreply.github.com>
2025-07-03 20:10:09 -0400

    smaller

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50">
-      <div className="flex items-center justify-between px-5 py-5 backdrop-blur-xl bg-black/50">
+      <div className="flex items-center justify-between px-5 py-3 backdrop-blur-xl bg-black/50">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="Crop Studio" width={62} height={62} className="h-16 w-16" />
